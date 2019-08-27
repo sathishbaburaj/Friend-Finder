149cc45165b9db97a7b40eb4dacddb7b68701e74
Sathish Pittala <48255299+sathishbaburaj@users.noreply.github.com>
2019-08-26 23:05:10 -0400

    Created app to find friends

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friends = [ {
+    "name" : "Andre",
+    "photo" : "Andre.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Charles",
+    "photo" : "Charles.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "David",
+    "photo" : "David.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Deepika",
+    "photo" : "Deepika.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Evlyn",
+    "photo" : "Evlyn.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Julie",
+    "photo" : "Julie",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Mac",
+    "photo" : "Mac.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Robert",
+    "photo" : "Robert.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Shilpa",
+    "photo" : "Shilpa.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Sonali",
+    "photo" : "Sonali.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}
+];
+
+module.exports = friends;
